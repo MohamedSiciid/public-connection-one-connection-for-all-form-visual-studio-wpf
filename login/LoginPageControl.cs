f97428fc4305375e9b81e957bcc10748d32f297f
MohamedSiciid <MohamedSiciid>
2024-05-04 00:36:10 +0300

    first commit

--- login/LoginPageControl.cs
+++ login/LoginPageControl.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Configuration;
 namespace login
 {
     public partial class LoginPageControl : UserControl
